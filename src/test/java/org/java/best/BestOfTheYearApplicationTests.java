36c15afb772c03b38c60cf0ebd5a887870c2544b
robertvlad <vladrob96@gmail.com>
2023-06-09 13:39:22 +0300

    Base file

---
+++ src/test/java/org/java/best/BestOfTheYearApplicationTests.java
@@ -0,0 +1,13 @@
+package org.java.best;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestOfTheYearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
